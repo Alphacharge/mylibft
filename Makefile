78b65f0934b11ab7be397a9c5e19c5804ebb5633
Alphacharge <rbetz@2-E-2.42heilbronn.de>
2022-06-02 09:49:43 +0200

    merged and cleaned libft with gnl and printf

--- Makefile
+++ Makefile
@@ -5,13 +5,22 @@
 #                                                     +:+ +:+         +:+      #
 #    By: rbetz <rbetz@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2022/04/14 13:46:08 by rbetz             #+#    #+#              #
-#    Updated: 2022/05/31 17:55:11 by rbetz            ###   ########.fr        #
+#    Created: 2022/03/25 14:21:20 by rbetz             #+#    #+#              #
+#    Updated: 2022/06/02 09:47:44 by rbetz            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = "libft.a"
-SRC = libft/*.c libft/*.h printf/*.c printf/*.h get_next_line/*.c get_next_line/*.h
+SRC = ft_atoi.c ft_bzero.c ft_calloc.c ft_isalnum.c ft_isalpha.c \
+		ft_isascii.c ft_isdigit.c ft_isprint.c ft_itoa.c ft_memchr.c \
+		ft_memcmp.c ft_memcpy.c ft_memmove.c ft_memset.c ft_putchar_fd.c \
+		ft_putendl_fd.c ft_putnbr_fd.c ft_putstr_fd.c ft_split.c \
+		ft_strchr.c ft_strdup.c ft_striteri.c ft_strjoin.c ft_strlcat.c \
+		ft_strlcpy.c ft_strlen.c ft_strmapi.c ft_strncmp.c ft_strnstr.c \
+		ft_strrchr.c ft_strtrim.c ft_substr.c ft_tolower.c ft_toupper.c \
+		ft_lstadd_back.c ft_lstadd_front.c ft_lstclear.c ft_lstdelone.c \
+		ft_lstiter.c ft_lstlast.c ft_lstmap.c ft_lstnew.c ft_lstsize.c \
+		ft_printf.c ft_printflibft.c get_next_line_bonus.c get_next_line_utils_bonus.c
 SRCOBJ=$(SRC:.c=.o)
 CC = "cc"
 CFLAGS = -Wall -Wextra -Werror
@@ -21,7 +30,7 @@ all: $(NAME)
 $(NAME): $(SRCOBJ)
 	ar -rcs $(NAME) $^
 
-clean:
+clean: 
 	/bin/rm -f $(SRCOBJ) libft.h.gch
 
 fclean: clean
---
+++ libft.h
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbetz <rbetz@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/25 11:33:22 by rbetz             #+#    #+#             */
+/*   Updated: 2022/06/02 09:44:41 by rbetz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdint.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_strlen(const char *s);
+int		ft_atoi(const char *str);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
+char	*ft_strdup(const char *s1);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n);
+char	**ft_split(char const *s, char c);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void	*));
+
+//printf
+# include <stdarg.h>
+
+int		ft_printf(const char *format, ...);
+int		ft_loop_format(const char *format, int chars, va_list args);
+int		ft_flaghandling(char c, va_list args);
+int		ft_hex(unsigned long number, char c);
+int		ft_counthex(unsigned long c);
+char	*ft_litoa(long n);
+int		ft_putchar_fd_l(char c, int fd);
+int		ft_putstr_fd_l(char *s, int fd, char c);
+
+//getnextline
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+//getnextline
+char	*get_next_line(int fd);
+void	*freeme(char **tofree);
+int		ft_posinset(char const c, char const *ptr);
+void	*hack_and_stack(char *line, char **cur_line, char **str_ret, int *pos);
+void	*read_and_join(char *line, char **cur_line, const int fd, int *ret);
+char	*ft_strjoin_ns(char *s1, char *s2);
+#endif

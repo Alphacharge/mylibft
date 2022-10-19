6bd5fd5a6e96b123808e32d454373c29bd1b6769
Alphacharge <rbetz@2-G-6.42heilbronn.de>
2022-10-19 13:01:43 +0200

    protected len again gt size

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rbetz <rbetz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 16:08:36 by rbetz             #+#    #+#             */
-/*   Updated: 2022/04/06 17:01:11 by rbetz            ###   ########.fr       */
+/*   Updated: 2022/10/19 13:00:34 by rbetz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (s == NULL)
 		return (NULL);
 	size = (size_t)ft_strlen(s);
+	if (len > size)
+		len = size;
 	if (start >= size)
 		return (ft_strdup(""));
 	ptr = malloc(len + 1);

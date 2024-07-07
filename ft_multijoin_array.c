ebcf5c5898a6914f3fb5b14157b9f573b9a2d77c
Alphacharge <git.mytechnikinfo.de>
2024-07-07 20:19:18 +0200

    added sizet array to avoid using strlen twice a string

--- ft_multijoin_array.c
+++ ft_multijoin_array.c
@@ -12,35 +12,40 @@
 
 #include "libft.h"
 
-static char	*copy_input(char **array, size_t len)
+static char	*copy_input(char **array, size_t *lengths, size_t len)
 {
 	char	*str;
 	size_t	i;
 	size_t	pos;
 
 	str = ft_calloc(len + 1, sizeof(char));
+	if (!str)
+		return (NULL);
 	i = 0;
 	pos = 0;
-	while (str != NULL && array[i] != NULL)
+	while (array[i] != NULL)
 	{
-		len = ft_strlen(array[i]);
-		ft_memcpy(&str[pos], array[i], len);
-		pos += len;
+		ft_memcpy(&str[pos], array[i], lengths[i]);
+		pos += lengths[i];
 		i++;
 	}
 	return (str);
 }
 
-static size_t	get_length(char **array)
+static size_t	get_total_length(char **array, size_t *lengths)
 {
-	size_t	len;
+	size_t	total_len;
 	size_t	i;
 
-	len = 0;
+	total_len = 0;
 	i = 0;
 	while (array[i] != NULL)
-		len += ft_strlen(array[i++]);
-	return (len);
+	{
+		lengths[i] = ft_strlen(array[i]);
+		total_len += lengths[i];
+		i++;
+	}
+	return (total_len);
 }
 
 /*Combines every string inside array into a single string. Array needs to be
@@ -49,12 +54,19 @@ Will not free any string inside the array, but frees the array.*/
 char	*ft_multijoin_array(char **array)
 {
 	size_t	len;
+	size_t	*lengths;
+	size_t	size;
 	char	*str;
 
 	if (array == NULL)
 		return (NULL);
-	len = get_length(array);
-	str = copy_input(array, len);
+	size = ft_arraycount(array);
+	lengths = ft_calloc(size, sizeof(size_t));
+	if (lengths == NULL)
+		return (NULL);
+	len = get_total_length(array, lengths);
+	str = copy_input(array, lengths, len);
+	free(lengths);
 	free(array);
 	return (str);
 }

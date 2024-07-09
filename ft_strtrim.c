f0c0f4b1b97171f30acd36e8f01e8ef6c066f1a5
Alphacharge <git.mytechnikinfo.de>
2024-07-09 11:45:25 +0200

    replaced isinset with strchr, because functionality is the same

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,18 +12,6 @@
 
 #include "libft.h"
 
-static int	ft_isinset(char const c, char const *ptr)
-{
-	int	i;
-
-	i = 0;
-	while (ptr[i] != c && ptr[i] != '\0')
-		i++;
-	if (ptr[i] != '\0')
-		return (1);
-	return (0);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		pos1;
@@ -35,9 +23,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (s1 == NULL || set == NULL)
 		return (NULL);
 	pos2 = ft_strlen(s1);
-	while (ft_isinset(s1[pos1], set))
+	while (ft_strchr(set, s1[pos1]) != NULL)
 		pos1++;
-	while (pos2 > pos1 && ft_isinset(s1[pos2 - 1], set))
+	while (pos2 > pos1 && ft_strchr(set, s1[pos2 - 1]) != NULL)
 		pos2--;
 	if ((pos2 - pos1) >= 0)
 		ptr = malloc(pos2 - pos1 + 1);

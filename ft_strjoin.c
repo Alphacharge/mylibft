5766b488946339a09c0330a37bd9b5e9b706a11c
Alphacharge <rbetz@student.42heilbronn.de>
2024-07-11 19:28:11 +0200

    killed 3 calles to strlen

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbetz <rbetz@student.42.fr>                +#+  +:+       +#+        */
+/*   By: humbi <humbi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 10:31:29 by rbetz             #+#    #+#             */
-/*   Updated: 2022/10/31 13:14:02 by rbetz            ###   ########.fr       */
+/*   Updated: 2024/07/11 19:27:39 by humbi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,17 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*p1;
+	size_t	len_s1;
+	size_t	len_s2;
 
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-	p1 = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	p1 = malloc((len_s1 + len_s2 + 1) * sizeof(char));
 	if (p1 == NULL)
 		return (NULL);
-	ft_memcpy(p1, s1, (size_t)ft_strlen(s1));
-	ft_memcpy(&p1[ft_strlen(s1)], s2, (size_t)ft_strlen(s2) + 1);
+	ft_memcpy(p1, s1, len_s1);
+	ft_memcpy(&p1[len_s1], s2, len_s2 + 1);
 	return (p1);
 }
